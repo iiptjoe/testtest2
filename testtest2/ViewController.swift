6919d8b1a605cc17b78b7cf3acbb6fa8db925d8c
iiptjoe <iip_tjoe@yahoo.co.id>
2019-05-15 08:21:19 +0700

    this is first commit

---
+++ testtest2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  testtest2
+//
+//  Created by M.Syarif Hidayat on 15/05/19.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ /// asdasd
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // adasdasd
+        
+        // Do any additional setup after loading the view.
+        print("this is my first git hub")
+    }
+
+
+}
+
